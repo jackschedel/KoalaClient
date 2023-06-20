ac4d130236a69fcfbcc04e97ee6e6d4334c24eee
Mario Dian <mariodian@gmail.com>
2023-06-20 14:23:24 +0800

    fix menubar icon size in macOS (#360)
    add support for MacOS theme (light/dark)

--- electron/index.cjs
+++ electron/index.cjs
@@ -1,11 +1,11 @@
-const {dialog,  app, ipcMain, BrowserWindow, Tray, Menu } = require('electron');
+const { dialog, app, ipcMain, BrowserWindow, Tray, Menu } = require('electron');
 
 process.on('uncaughtException', (error) => {
-  // Perform any necessary cleanup tasks here
-  dialog.showErrorBox('An error occurred', error.stack);
+   // Perform any necessary cleanup tasks here
+   dialog.showErrorBox('An error occurred', error.stack);
 
-  // Exit the app
-  process.exit(1);
+   // Exit the app
+   process.exit(1);
 });
 
 const path = require('path');
@@ -16,201 +16,199 @@ let closeToTray = false;
 let winTray = null;
 let trayExists = false;
 const instanceLock = app.requestSingleInstanceLock();
+const isMacOS = process.platform === 'darwin';
 
 if (require('electron-squirrel-startup')) app.quit();
 
 const PORT = isDev ? '5173' : '51735';
+const ICON = 'icon-rounded.png';
+const ICON_TEMPLATE = 'iconTemplate.png';
 
-function handleSetCloseToTray (event, setting) {
-  closeToTray = setting;
+function handleSetCloseToTray(event, setting) {
+   closeToTray = setting;
 
-  if(closeToTray && trayExists){
-    winTray.destroy();
-    trayExists = false;
-  }
+   if (closeToTray && trayExists) {
+      winTray.destroy();
+      trayExists = false;
+   }
 
-  if(closeToTray && !trayExists){
-    createTray(win);
-    trayExists = true;
-  }
+   if (closeToTray && !trayExists) {
+      createTray(win);
+      trayExists = true;
+   }
 }
 
 function createWindow() {
-  let iconPath = '';
-  if (isDev) {
-    iconPath = path.join(__dirname, '../public/icon-rounded.png');
-  } else {
-    iconPath = path.join(__dirname, '../dist/icon-rounded.png');
-  }
-  autoUpdater.checkForUpdatesAndNotify();
+   autoUpdater.checkForUpdatesAndNotify();
 
-  win = new BrowserWindow({
-	  autoHideMenuBar: true,
-    show: false,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      nativeWindowOpen: true,
-    }
-  });
+   win = new BrowserWindow({
+      autoHideMenuBar: true,
+      show: false,
+      icon: assetPath(ICON),
+   });
 
-  win.maximize();
-  win.show();
+   win.maximize();
+   win.show();
 
-  isDev || createServer();
+   isDev || createServer();
 
-  win.loadURL(`http://localhost:${PORT}`);
+   win.loadURL(`http://localhost:${PORT}`);
 
-  if (isDev) {
-    win.webContents.openDevTools({ mode: 'detach' });
-  }
+   if (isDev) {
+      win.webContents.openDevTools({ mode: 'detach' });
+   }
 
-  win.on('close', function (event) {
-    if(closeToTray && !app.isQuiting){
-        event.preventDefault();
-        win.hide();
-    }
-  });
+   win.on('close', function (event) {
+      if (closeToTray && !app.isQuiting) {
+         event.preventDefault();
+         win.hide();
+      }
+   });
 
-  win.on('show', function (event) {
-    win.maximize();
-    win.focus();
-  });
+   win.on('show', function (event) {
+      win.maximize();
+      win.focus();
+   });
 
-  return win;
+   return win;
+}
+
+const assetPath = (asset) => {
+   return path.join(
+      __dirname,
+      isDev ? `../public/${asset}` : `../dist/${asset}`
+   )
 }
 
 const createTray = (window) => {
-  winTray = Tray(
-    path.join(
-      __dirname,
-      isDev ? '../public/icon-rounded.png' : '../dist/icon-rounded.png'
-    )
-  );
-  const contextMenu = Menu.buildFromTemplate([
-    {
-      label: 'Show',
-      click: () => {
-        if (win) {
-          if (!win.isVisible()) win.show()
-    
-          if (win.isMinimized()) win.restore()
-          
-          win.focus()
-        }
+   const tray = new Tray(
+      assetPath(!isMacOS ? ICON : ICON_TEMPLATE)
+   );
+   const contextMenu = Menu.buildFromTemplate([
+      {
+         label: 'Show',
+         click: () => {
+            if (win) {
+               if (!win.isVisible()) win.show()
+
+               if (win.isMinimized()) win.restore()
+
+               win.focus()
+            }
+         },
       },
-    },
-    {
-      label: 'Exit',
-      click: () => {
-        app.isQuiting = true;
-        app.quit();
+      {
+         label: 'Exit',
+         click: () => {
+            app.isQuiting = true;
+            app.quit();
+         },
       },
-    },
-  ]);
+   ]);
 
-  winTray.on('click', () => {
-    if (win) {
-      if (!win.isVisible()) win.show()
+   winTray.on('click', () => {
+      if (win) {
+         if (!win.isVisible()) win.show()
 
-      if (win.isMinimized()) win.restore()
-      
-      win.focus()
-    }
-  });
-  winTray.setToolTip('Better ChatGPT');
-  winTray.setContextMenu(contextMenu);
+         if (win.isMinimized()) win.restore()
 
-  return winTray;
+         win.focus()
+      }
+   });
+   winTray.setToolTip('Better ChatGPT');
+   winTray.setContextMenu(contextMenu);
+
+   return winTray;
 };
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+   if (!isMacOS) {
+      app.quit();
+   }
 });
 
 if (!instanceLock) {
-  app.quit()
+   app.quit()
 } else {
-  app.on('second-instance', (event, commandLine, workingDirectory) => {
-    if (win) {
-      if (!win.isVisible()) win.show()
+   app.on('second-instance', (event, commandLine, workingDirectory) => {
+      if (win) {
+         if (!win.isVisible()) win.show()
 
-      if (win.isMinimized()) win.restore()
+         if (win.isMinimized()) win.restore()
 
-      win.focus()
-    }
-  })
+         win.focus()
+      }
+   })
 
-  app.whenReady().then(() => {
-    ipcMain.on('set-close-to-tray', handleSetCloseToTray)
+   app.whenReady().then(() => {
+      ipcMain.on('set-close-to-tray', handleSetCloseToTray)
 
-    win = createWindow()
-  })
+      win = createWindow()
+   })
 }
 
 const createServer = () => {
-  // Dependencies
-  const http = require('http');
-  const fs = require('fs');
-  const path = require('path');
+   // Dependencies
+   const http = require('http');
+   const fs = require('fs');
+   const path = require('path');
 
-  // MIME types for different file extensions
-  const mimeTypes = {
-    '.html': 'text/html',
-    '.css': 'text/css',
-    '.js': 'text/javascript',
-    '.wasm': 'application/wasm',
-    '.jpg': 'image/jpeg',
-    '.jpeg': 'image/jpeg',
-    '.png': 'image/png',
-    '.gif': 'image/gif',
-    '.svg': 'image/svg+xml',
-    '.json': 'application/json',
-  };
+   // MIME types for different file extensions
+   const mimeTypes = {
+      '.html': 'text/html',
+      '.css': 'text/css',
+      '.js': 'text/javascript',
+      '.wasm': 'application/wasm',
+      '.jpg': 'image/jpeg',
+      '.jpeg': 'image/jpeg',
+      '.png': 'image/png',
+      '.gif': 'image/gif',
+      '.svg': 'image/svg+xml',
+      '.json': 'application/json',
+   };
 
-  // Create a http server
-  const server = http.createServer((request, response) => {
-    // Get the file path from the URL
-    let filePath =
-      request.url === '/'
-        ? `${path.join(__dirname, '../dist/index.html')}`
-        : `${path.join(__dirname, `../dist/${request.url}`)}`;
+   // Create a http server
+   const server = http.createServer((request, response) => {
+      // Get the file path from the URL
+      let filePath =
+         request.url === '/'
+            ? `${path.join(__dirname, '../dist/index.html')}`
+            : `${path.join(__dirname, `../dist/${request.url}`)}`;
 
-    // Get the file extension from the filePath
-    let extname = path.extname(filePath);
+      // Get the file extension from the filePath
+      let extname = path.extname(filePath);
 
-    // Set the default MIME type to text/plain
-    let contentType = 'text/plain';
+      // Set the default MIME type to text/plain
+      let contentType = 'text/plain';
 
-    // Check if the file extension is in the MIME types object
-    if (extname in mimeTypes) {
-      contentType = mimeTypes[extname];
-    }
-
-    // Read the file from the disk
-    fs.readFile(filePath, (error, content) => {
-      if (error) {
-        // If file read error occurs
-        if (error.code === 'ENOENT') {
-          // File not found error
-          response.writeHead(404);
-          response.end('File Not Found');
-        } else {
-          // Server error
-          response.writeHead(500);
-          response.end(`Server Error: ${error.code}`);
-        }
-      } else {
-        // File read successful
-        response.writeHead(200, { 'Content-Type': contentType });
-        response.end(content, 'utf-8');
+      // Check if the file extension is in the MIME types object
+      if (extname in mimeTypes) {
+         contentType = mimeTypes[extname];
       }
-    });
-  });
 
-  // Listen for request on port ${PORT}
-  server.listen(PORT, () => {
-    console.log(`Server listening on http://localhost:${PORT}/`);
-  });
+      // Read the file from the disk
+      fs.readFile(filePath, (error, content) => {
+         if (error) {
+            // If file read error occurs
+            if (error.code === 'ENOENT') {
+               // File not found error
+               response.writeHead(404);
+               response.end('File Not Found');
+            } else {
+               // Server error
+               response.writeHead(500);
+               response.end(`Server Error: ${error.code}`);
+            }
+         } else {
+            // File read successful
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+         }
+      });
+   });
+
+   // Listen for request on port ${PORT}
+   server.listen(PORT, () => {
+      console.log(`Server listening on http://localhost:${PORT}/`);
+   });
 };
