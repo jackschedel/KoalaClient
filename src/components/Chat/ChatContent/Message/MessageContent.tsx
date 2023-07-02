2e8105b146e5f871eef5872587890197c301a54f
cheeseonamonkey <cheeseonamonkey@gmail.com>
2023-07-01 23:38:15 -0600

    - adjusted some sizes - swapped places of the delete bttons ( ✘  ✅) in chat history

--- src/components/Chat/ChatContent/Message/MessageContent.tsx
+++ src/components/Chat/ChatContent/Message/MessageContent.tsx
@@ -5,39 +5,39 @@ import ContentView from './View/ContentView';
 import EditView from './View/EditView';
 
 const MessageContent = ({
-  role,
-  content,
-  messageIndex,
-  sticky = false,
+   role,
+   content,
+   messageIndex,
+   sticky = false,
 }: {
-  role: string;
-  content: string;
-  messageIndex: number;
-  sticky?: boolean;
+   role: string;
+   content: string;
+   messageIndex: number;
+   sticky?: boolean;
 }) => {
-  const [isEdit, setIsEdit] = useState<boolean>(sticky);
-  const advancedMode = useStore((state) => state.advancedMode);
+   const [isEdit, setIsEdit] = useState<boolean>(sticky);
+   const advancedMode = useStore((state) => state.advancedMode);
 
-  return (
-    <div className='relative flex flex-col gap-2 md:gap-3 lg:w-[calc(100%-115px)]'>
-      {advancedMode && <div className='flex flex-grow flex-col gap-3'></div>}
-      {isEdit ? (
-        <EditView
-          content={content}
-          setIsEdit={setIsEdit}
-          messageIndex={messageIndex}
-          sticky={sticky}
-        />
-      ) : (
-        <ContentView
-          role={role}
-          content={content}
-          setIsEdit={setIsEdit}
-          messageIndex={messageIndex}
-        />
-      )}
-    </div>
-  );
+   return (
+      <div className='relative flex flex-col gap-2 md:gap-3 lg:w-[calc(100%-100px)]'>
+         {advancedMode && <div className='flex flex-grow flex-col gap-3'></div>}
+         {isEdit ? (
+            <EditView
+               content={content}
+               setIsEdit={setIsEdit}
+               messageIndex={messageIndex}
+               sticky={sticky}
+            />
+         ) : (
+            <ContentView
+               role={role}
+               content={content}
+               setIsEdit={setIsEdit}
+               messageIndex={messageIndex}
+            />
+         )}
+      </div>
+   );
 };
 
 export default MessageContent;
