7eb9fcb46dff4d154f4fc918e820a071a253cf8d
Jack Schedel <jackschedel@gmail.com>
2023-08-10 22:06:37 -0400

    folder new chat style revamp

--- src/components/Menu/NewChat.tsx
+++ src/components/Menu/NewChat.tsx
@@ -13,28 +13,20 @@ const NewChat = ({ folder }: { folder?: string }) => {
 
   return (
     <a
-      className={`flex flex-1 items-center rounded-md hover:bg-neutral-light transition-all duration-200 text-white text-sm flex-shrink-0 ${
+      className={`flex flex-1 items-center rounded-md hover:bg-neutral-light transition-all duration-200 flex-shrink-0 p-2 ${
         generating
           ? 'cursor-not-allowed opacity-40'
           : 'cursor-pointer opacity-100'
       } ${
-        folder ? 'justify-start' : 'py-2 px-2 gap-3 mb-2 border border-white/20'
+        folder ? 'justify-start gap-3 text-custom-white/50 hover:text-custom-white' : 'text-custom-white gap-2 mb-2 border border-white/20'
       }`}
       onClick={() => {
         if (!generating) addChat(folder);
       }}
-      title={folder ? String(t('newChat')) : ''}
+      title={String(t('newChat'))}
     >
-      {folder ? (
-        <div className='max-h-0 parent-sibling-hover:max-h-10 hover:max-h-10 parent-sibling-hover:py-2 hover:py-2 px-2 overflow-hidden transition-all duration-200 delay-500 text-sm flex gap-3 items-center text-custom-white'>
-          <PlusIcon /> {t('newChat')}
-        </div>
-      ) : (
-        <>
-          <PlusIcon />
-          <span className='inline-flex text-white text-sm'>{t('newChat')}</span>
-        </>
-      )}
+      <PlusIcon />
+      <span className='inline-flex text-sm'>{t('newChat')}</span>
     </a>
   );
 };
