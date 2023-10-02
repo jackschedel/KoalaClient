1e015b81e61224ef28f2a470980386545c667af0
Danil Yarantsev (Yardanico) <tiberiumk12@gmail.com>
2023-10-02 23:49:05 +0300

    Fix SSE parsing

--- src/api/helper.ts
+++ src/api/helper.ts
@@ -1,17 +1,14 @@
-import { EventSourceData } from '@type/api';
+import { EventSourceData } from "@type/api";
 
 export const parseEventSource = (
   data: string
 ): '[DONE]' | EventSourceData[] => {
   const result = data
-    .split('\n\n')
-    .filter(Boolean)
-    .map((chunk) => {
-      const jsonString = chunk
         .split('\n')
-        .map((line) => line.replace(/^data: /, ''))
-        .join('');
-      if (jsonString === '[DONE]') return jsonString;
+    .filter(line => line.startsWith('data: ') || line === '[DONE]')
+    .map((line) => {
+      if (line === '[DONE]') return line;
+      const jsonString = line.replace(/^data: /, '');
       try {
         const json = JSON.parse(jsonString);
         return json;
@@ -20,26 +17,4 @@ export const parseEventSource = (
       }
     });
   return result;
-};
-
-export const createMultipartRelatedBody = (
-  metadata: object,
-  file: File,
-  boundary: string
-): Blob => {
-  const encoder = new TextEncoder();
-
-  const metadataPart = encoder.encode(
-    `--${boundary}\r\nContent-Type: application/json; charset=UTF-8\r\n\r\n${JSON.stringify(
-      metadata
-    )}\r\n`
-  );
-  const filePart = encoder.encode(
-    `--${boundary}\r\nContent-Type: ${file.type}\r\n\r\n`
-  );
-  const endBoundary = encoder.encode(`\r\n--${boundary}--`);
-
-  return new Blob([metadataPart, filePart, file, endBoundary], {
-    type: 'multipart/related; boundary=' + boundary,
-  });
 };
