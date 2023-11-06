c780deab23592020255ed532c3803586d8168c34
Danil Yarantsev (Yardanico) <tiberiumk12@gmail.com>
2023-11-07 02:48:22 +0300

    Add gpt-4 turbo preview

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -20,6 +20,7 @@ Respond using Markdown.`;
 export const modelOptions: ModelChoice[] = [
   'gpt-3.5-turbo',
   'gpt-3.5-turbo-16k',
+  'gpt-4-1106-preview',
   'gpt-4',
   'gpt-4-32k',
   'claude-2',
@@ -40,6 +41,7 @@ export const modelMaxToken = {
   'gpt-4': 8192,
   'gpt-4-0314': 8192,
   'gpt-4-0613': 8192,
+  'gpt-4-1106-preview': 128000,
   'gpt-4-32k': 32768,
   'gpt-4-32k-0314': 32768,
   'gpt-4-32k-0613': 32768,
@@ -79,6 +81,10 @@ export const modelCost = {
   'gpt-4-0613': {
     prompt: { price: 0.03, unit: 1000 },
     completion: { price: 0.06, unit: 1000 },
+  },
+  'gpt-4-1106-preview': {
+    prompt: { price: 0.01, unit: 1000 },
+    completion: { price: 0.03, unit: 1000 }
   },
   'gpt-4-32k': {
     prompt: { price: 0.06, unit: 1000 },
--- src/types/chat.ts
+++ src/types/chat.ts
@@ -53,6 +53,7 @@ export interface Folder {
 export type ModelChoice =
   | 'gpt-4'
   | 'gpt-4-32k'
+  | 'gpt-4-1106-preview'
   | 'gpt-3.5-turbo'
   | 'gpt-3.5-turbo-16k'
   | 'claude-2'
