ca21a2dd6da1b566e9b4125a7de3a45978bbc37e
Jack Schedel <jackschedel@gmail.com>
2024-01-05 23:55:31 -0500

    whisper uses generating lock + stop generating button has onClick rather than whole div fixes #87

--- src/components/Chat/ChatContent/Message/WhisperRecord.tsx
+++ src/components/Chat/ChatContent/Message/WhisperRecord.tsx
@@ -14,16 +14,17 @@ const WhisperRecord = ({
   messageIndex: number;
 }) => {
   let apiKey = useStore((state) => state.apiKey);
-
+  const setGenerating = useStore((state) => state.setGenerating);
   apiKey = apiKey || '0';
 
   const { transcript, startRecording, stopRecording } = useWhisper({ apiKey });
 
   useEffect(() => {
-    if (transcript.text) {
+    if (transcript.text != null) {
       _setContent((prev) => {
         return prev.replace('◯', transcript.text || '');
       });
+      setGenerating(false);
     }
   }, [transcript.text]);
 
@@ -54,6 +55,7 @@ const WhisperRecord = ({
         return startContent + paddedStart + '◉' + paddedEnd + endContent;
       });
       startRecording();
+      setGenerating(true);
     }
     setIsRecording(!isRecording);
   };
