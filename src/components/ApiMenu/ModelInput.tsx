ce09786aebdec44a38f92663574e0eff687e5074
cheeseonamonkey <cheeseonamonkey@gmail.com>
2024-03-30 16:58:53 -0600

    add titles to input fields (hover popup labels)

--- src/components/ApiMenu/ModelInput.tsx
+++ src/components/ApiMenu/ModelInput.tsx
@@ -32,33 +32,36 @@ export const ModelInput: React.FC<ModelInputProps> = ({
     const handleModelChange = (field: string, value: string | number | boolean) => {
         _setModelDef(prev => ({ ...prev, [field]: value }));
     };
-
     return (
-        <div key={`model${index}`} className='mb-4'>
+        <div key={`model${index}`} className='mb-4 p-1 pb-4 border-b border-gray-600/50 shadow-inner'>
             <div className='flex items-center border-b border-neutral-base/50 mb-1 p-1'>
                 <div className='flex-1'>
                     <input
                         type='text'
-                        className='text-custom-black p-3 text-sm border-none bg-custom-white rounded-md m-0 w-full mr-0 h-8 focus:outline-none'
+                        className='text-custom-black p-2 text-sm border-none bg-custom-white rounded-md m-0 w-full mr-0 h-8 focus:outline-none'
                         placeholder='Nickname'
                         value={_modelDef.name}
                         onChange={e => handleModelChange('name', e.target.value)}
+                        title='Nickname'
                     />
                 </div>
                 <div className='flex-1 px-1'>
                     <input
                         type='text'
-                        className='text-custom-black p-3 text-sm border-none bg-custom-white rounded-md m-0 w-full mr-0 h-8 focus:outline-none'
+                        className='text-custom-black p-2 text-sm border-none bg-custom-white rounded-md m-0 w-full mr-0 h-8 focus:outline-none'
                         placeholder='Model'
                         value={_modelDef.model}
                         onChange={e => handleModelChange('model', e.target.value)}
+                        title='Model'
                     />
                 </div>
                 <div className='flex-1'>
                     <button
                         className='btn bg-custom-white text-custom-black btn-small overflow-clip relative pr-6 w-80'
                         type='button'
-                        onClick={() => _setActiveDropdown(!_activeDropdown)}>
+                        onClick={() => _setActiveDropdown(!_activeDropdown)}
+                        title='Endpoint Dropdown'
+                    >
                         <span className='inline-block truncate max-w-full'>
                             {_modelDef.endpoint !== undefined && apiAuth[_modelDef.endpoint]
                                 ? apiAuth[_modelDef.endpoint].endpoint.replace(/^https?:\/\//, '')
@@ -78,7 +81,8 @@ export const ModelInput: React.FC<ModelInputProps> = ({
                                         setModelEndpoint(index, authIndex);
                                         _setActiveDropdown(false);
                                     }}
-                                    key={`dropdown${authIndex}`}>
+                                    key={`dropdown${authIndex}`}
+                                >
                                     {auth.endpoint.replace(/^https?:\/\//, '')}
                                 </li>
                             ))}
@@ -94,52 +98,56 @@ export const ModelInput: React.FC<ModelInputProps> = ({
                     <input
                         type='text'
                         pattern='[0-9]*'
-                        className='text-custom-black p-3 text-sm border-none bg-custom-white rounded-md m-0 w-full mr-0 h-8 focus:outline-none'
+                        className='text-custom-black p-2 text-sm border-none bg-custom-white rounded-md m-0 w-full mr-0 h-8 focus:outline-none'
                         placeholder='Max Tokens'
                         value={_modelDef.model_max_tokens || ''}
                         onChange={e => {
                             const value = Number(e.target.value);
                             if (!isNaN(value)) handleModelChange('model_max_tokens', value);
                         }}
+                        title='Max Tokens'
                     />
                 </div>
                 <div className='flex-1 pr-1'>
                     <input
                         type='text'
                         pattern='[0-9]*'
-                        className='text-custom-black p-3 text-sm border-none bg-custom-white rounded-md m-0 w-full mr-0 h-8 focus:outline-none'
+                        className='text-custom-black p-2 text-sm border-none bg-custom-white rounded-md m-0 w-full mr-0 h-8 focus:outline-none'
                         placeholder='Max Context'
                         value={_modelDef.model_max_context || ''}
                         onChange={e => {
                             const value = Number(e.target.value);
                             if (!isNaN(value)) handleModelChange('model_max_context', value);
                         }}
+                        title='Max Context'
                     />
                 </div>
                 <div className='flex-1 pr-1'>
                     <input
                         type='text'
                         pattern='[0-9]*'
-                        className='text-custom-black p-3 text-sm border-none bg-custom-white rounded-md m-0 w-full mr-0 h-8 focus:outline-none'
+                        className='text-custom-black p-2 text-sm border-none bg-custom-white rounded-md m-0 w-full mr-0 h-8 focus:outline-none'
                         placeholder='Prompt Cost*'
                         value={_modelDef?.prompt_cost_1000 || ''}
                         onChange={e => {
                             const value = Number(e.target.value);
                             if (!isNaN(value)) handleModelChange('prompt_cost_1000', value);
                         }}
+                        title='Prompt Cost'
                     />
                 </div>
                 <div className='flex-1'>
                     <input
                         type='text'
                         pattern='[0-9]*'
-                        className='text-custom-black p-3 text-sm border-none bg-custom-white rounded-md m-0 w-full mr-0 h-8 focus:outline-none'
+                        className='text-custom-black p-2 text-sm border-none bg-custom-white rounded-md m-0 w-full mr-0 h-8 focus:outline-none'
                         placeholder='Completion Cost*'
                         value={_modelDef.completion_cost_1000 || ''}
                         onChange={e => {
                             const value = Number(e.target.value);
                             if (!isNaN(value)) handleModelChange('completion_cost_1000', value);
                         }}
+                        title='Completion Cost'
                     />
                 </div>
                 <div className='p-1 ml-2 hover:text-custom-black hover:bg-custom-white hover:rounded'>
@@ -148,4 +156,5 @@ export const ModelInput: React.FC<ModelInputProps> = ({
             </div>
         </div>
     );
+
 };
